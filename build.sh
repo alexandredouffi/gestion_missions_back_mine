57685c7560cac0047b566d55a560bd1d3f3870be
DOUFFI Kouame Alexandre <alexandrekdouffi@gmail.com>
2026-08-21 10:36:53 +0000

    maj loadata

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install -r ../requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate 
-python manage.py flush --no-input
-python manage.py loaddata data
+# python manage.py collectstatic --noinput
+# python manage.py migrate 
+# python manage.py flush --no-input
+# python manage.py loaddata data
